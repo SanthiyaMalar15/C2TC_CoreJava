0045ed45690c4bd28cf333bf01ea29de94a02e54
LENOVO <LENOVO@DESKTOP-RULABRI>
2024-08-06 06:24:20 -0700

    interfaceoperation

---
+++ src/santhiyadaynineteen/InterfaceOperation.java
@@ -0,0 +1,10 @@
+package santhiyadaynineteen;
+
+public class InterfaceOperation {
+	float performArithmetic(int a, int b) {
+		return 0;
+	}
+	}
+
+
+
---
+++ src/santhiyadaynineteen/MyCube.java
@@ -0,0 +1,9 @@
+package santhiyadaynineteen;
+
+public abstract class MyCube {
+	abstract int getCube(int no);
+	//void show(); not allow more than one abstract method
+}
+
+
+
