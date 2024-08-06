b7d8cc7fe2036d312dfb72637b07a0cba31ce608
LENOVO <LENOVO@DESKTOP-RULABRI>
2024-08-06 07:00:40 -0700

    multiplecatchdemo

---
+++ src/santhiyadayeleven/Demo.java
@@ -0,0 +1,16 @@
+package santhiyadayeleven;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+
+
+
+
+	}
+
+
