066f2d2917698322dd8c8de9e7fcb0126428317b
LENOVO <LENOVO@DESKTOP-RULABRI>
2024-08-06 06:53:36 -0700

    myinterface

---
+++ src/santhiyadayeight/Interface.java
@@ -0,0 +1,11 @@
+package santhiyadayeight;
+
+public class Interface {
+	public interface Phone {
+		void call();
+
+		void sms();
+	}
+
+
+}
---
+++ src/santhiyadayeight/MyInterFace.java
@@ -0,0 +1,12 @@
+package santhiyadayeight;
+
+public class MyInterFace {
+	public interface MyInterface {
+		 void calculateArea();
+		    interface MyInnerInterface {
+		       int  id = 20;
+		       void print();     
+		    }
+	}
+
+}
